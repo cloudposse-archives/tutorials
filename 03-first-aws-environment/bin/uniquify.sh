908e4d310938f35d0222eae25d7570c2fb6c66a1
Matt Gowie <matt@masterpoint.io>
2021-04-18 15:31:13 -0600

    feat: updates stacks to include TODOs for names + adds bin/uniquify.sh

---
+++ 03-first-aws-environment/bin/uniquify.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Since we're creating an S3 bucket as part of this tutorial, we need the names of those
+# buckets to be unique, otherwise we'll run into issues with AWS S3 API errors blocking
+# our terraform applies. This scripts uniquifies all bucket names in the project.
+
+# Build random value
+RAND=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 12 | head -n 1)
+PROD_RAND=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 12 | head -n 1)
+
+# Updates our tfstate-backend's name to be unique
+yq eval --inplace \
+    ".components.terraform.tfstate-backend.vars.name = \"tfstate-$RAND\"" \
+    stacks/uw2-root.yaml
+
+# Updates our static backend config with unique names
+yq eval --inplace \
+    ".terraform.backend.s3.bucket = \"acme-uw2-tfstate-$RAND\"" \
+    stacks/catalog/globals.yaml
+
+yq eval --inplace \
+    ".terraform.backend.s3.dynamodb_table = \"acme-uw2-tfstate-$RAND-lock\"" \
+    stacks/catalog/globals.yaml
+
+# Updates our dev + prod static-site component vars to ensure we're creating unique names
+yq eval --inplace \
+    ".components.terraform.static-site.vars.attributes.[0] = \"$RAND\"" \
+    stacks/uw2-dev.yaml
+
+yq eval --inplace \
+    ".components.terraform.static-site.vars.attributes.[0] = \"$PROD_RAND\"" \
+    stacks/uw2-prod.yaml
