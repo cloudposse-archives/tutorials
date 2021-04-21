b6a59e16e7e8a175cbe247873ae360e087485276
Matt Gowie <matt@masterpoint.io>
2021-04-21 10:29:21 -0600

    fix: issue with dynamo lock attributes being out of order

--- 03-first-aws-environment/bin/random-pet.sh
+++ 03-first-aws-environment/bin/random-pet.sh
@@ -7,27 +7,27 @@
 
 # Build random value
 # We install golang-petname as part of our tutorials toolbox.
-RAND=$(golang-petname)
+RANDOM_PET=$(golang-petname)
 
 # Updates our tfstate-backend's attributes to be unique
 yq eval --inplace \
-    ".components.terraform.tfstate-backend.vars.attributes.[0] = \"$RAND\"" \
+    ".components.terraform.tfstate-backend.vars.attributes.[0] = \"$RANDOM_PET\"" \
     stacks/ue2-root.yaml
 
 # Updates our static backend config with unique names
 yq eval --inplace \
-    ".terraform.backend.s3.bucket = \"acme-uw2-tfstate-$RAND\"" \
+    ".terraform.backend.s3.bucket = \"acme-uw2-tfstate-$RANDOM_PET\"" \
     stacks/catalog/globals.yaml
 
 yq eval --inplace \
-    ".terraform.backend.s3.dynamodb_table = \"acme-uw2-tfstate-$RAND-lock\"" \
+    ".terraform.backend.s3.dynamodb_table = \"acme-uw2-tfstate-lock-$RANDOM_PET\"" \
     stacks/catalog/globals.yaml
 
 # Updates our dev + prod static-site component vars to ensure we're creating unique names
 yq eval --inplace \
-    ".components.terraform.static-site.vars.attributes.[0] = \"$RAND\"" \
+    ".components.terraform.static-site.vars.attributes.[0] = \"$RANDOM_PET\"" \
     stacks/uw2-dev.yaml
 
 yq eval --inplace \
-    ".components.terraform.static-site.vars.attributes.[0] = \"$RAND\"" \
+    ".components.terraform.static-site.vars.attributes.[0] = \"$RANDOM_PET\"" \
     stacks/uw2-prod.yaml
