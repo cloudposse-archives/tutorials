49b2c22fc828435deced1c87491d962a176eb655
Matt Gowie <matt@masterpoint.io>
2021-04-21 10:03:44 -0600

    feat: updates to use golang-petname > random string

--- 03-first-aws-environment/bin/random-pet.sh
+++ 03-first-aws-environment/bin/random-pet.sh
@@ -2,15 +2,16 @@
 #
 # Since we're creating an S3 bucket as part of this tutorial, we need the names of those
 # buckets to be unique, otherwise we'll run into issues with AWS S3 API errors blocking
-# our terraform applies. This scripts uniquifies all bucket names in the project.
+# our terraform applies. This scripts generates random pet names to append to attributes
+# which ensures all bucket names in the project are unique.
 
 # Build random value
+# We install golang-petname as part of our tutorials toolbox.
 RAND=$(golang-petname)
-PROD_RAND=$(golang-petname)
 
-# Updates our tfstate-backend's name to be unique
+# Updates our tfstate-backend's attributes to be unique
 yq eval --inplace \
-    ".components.terraform.tfstate-backend.vars.name = \"tfstate-$RAND\"" \
+    ".components.terraform.tfstate-backend.vars.attributes.[0] = \"$RAND\"" \
     stacks/ue2-root.yaml
 
 # Updates our static backend config with unique names
@@ -28,5 +29,5 @@ yq eval --inplace \
     stacks/uw2-dev.yaml
 
 yq eval --inplace \
-    ".components.terraform.static-site.vars.attributes.[0] = \"$PROD_RAND\"" \
+    ".components.terraform.static-site.vars.attributes.[0] = \"$RAND\"" \
     stacks/uw2-prod.yaml
