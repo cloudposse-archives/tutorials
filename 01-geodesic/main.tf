e063e6e1ad24e10f3b9170f34e130ebc1ab5a70e
milldr <miller0daniel@gmail.com>
2022-10-07 14:27:33 -0700

    updated api to working example

--- 01-geodesic/main.tf
+++ 01-geodesic/main.tf
@@ -1,11 +1,11 @@
-data "http" "star_wars" {
-  url = "https://swapi.dev/api/people/1"
+data "http" "cat_facts" {
+  url = "https://catfact.ninja/fact"
   request_headers = {
     Accept = "application/json"
   }
 }
 
-output "star_wars_data" {
-  value       = jsondecode(data.http.star_wars.body)
-  description = "Star wars data that we output as part of this simple example project"
+output "cat_facts_data" {
+  value       = jsondecode(data.http.cat_facts.body)
+  description = "Cat Fact data that we output as part of this simple example project"
 }
