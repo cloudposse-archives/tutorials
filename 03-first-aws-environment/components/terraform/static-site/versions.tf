cb17dfeea34d8abe97dd4c44b209777a8a1d4206
milldr <miller0daniel@gmail.com>
2023-06-06 12:24:33 -0400

    comment on v5 pinnning

--- 03-first-aws-environment/components/terraform/static-site/versions.tf
+++ 03-first-aws-environment/components/terraform/static-site/versions.tf
@@ -4,6 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      # Note, we are not upgrading to v5 until this issue is resolved:
+      # https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/issues/279
       version = "< 5"
     }
   }
